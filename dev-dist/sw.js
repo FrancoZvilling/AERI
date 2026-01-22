0daa1185c1cc04fe2718c0aecdef76ac97fdce09
Franco <francozvilling2014@hotmail.com>
2026-01-22 14:55:03 -0300

    avatar pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9r9ia5falfg"
+    "revision": "0.p9jmp9vc6s8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
